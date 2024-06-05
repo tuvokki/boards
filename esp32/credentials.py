5470af24cad8a2f2e954078337252712a643c6f6
Wouter Roosendaal <wouterroosendaal@gmail.com>
2024-06-05 13:59:49 +0200

    Update credentials instructions

--- esp32/credentials.py
+++ esp32/credentials.py
@@ -6,7 +6,11 @@ class Credentials:
 
     If a change is needed, when for instance credentials are added, run the following command:
         `git update-index --no-skip-worktree esp32/credentials.py`
-
+    Save your edits and revert the changes to the latest HEAD:
+        `git checkout HEAD -- esp32/credentials.py`
+    Add stuff that is devoid of any secrets, and lock the file again.
+        `git update-index --skip-worktree esp32/credentials.py`
+    Now restate the secrets you added and add the new ones.
     """
     SSID = "[[wifi network name]]"
     PASSWORD = "[[wifi network pass]]"
