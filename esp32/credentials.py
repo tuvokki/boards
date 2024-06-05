cb695ac11dcbaace93af79b5b88d824f785267ef
Wouter Roosendaal <wouterroosendaal@gmail.com>
2024-06-05 13:54:26 +0200

    Update credentials instructions

--- esp32/credentials.py
+++ esp32/credentials.py
@@ -1,7 +1,12 @@
 class Credentials:
     """
-    This class holds all secrest used in this project.
-    After change beware of accidentally committing this to git so this is added to .gitignore
+    This class holds all secrets used in this project.
+    After change beware of accidentally committing this to git run the following command:
+        `git update-index --skip-worktree esp32/credentials.py`
+
+    If a change is needed, when for instance credentials are added, run the following command:
+        `git update-index --no-skip-worktree esp32/credentials.py`
+
     """
     SSID = "[[wifi network name]]"
     PASSWORD = "[[wifi network pass]]"
